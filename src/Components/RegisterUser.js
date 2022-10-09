8d586a65ba01fd4c559ca2a03e963eeb08373ce2
Liam Wedge <21100218@virtualwindow.co.za>
2022-10-09 11:25:42 +0200

    register and authenticate

--- src/Components/RegisterUser.js
+++ src/Components/RegisterUser.js
@@ -1,11 +1,23 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "../CSS/Register.css";
+import { useNavigate } from "react-router-dom";
 import regImage from "../Assets/registerImage.png";
 import { useState } from "react";
+import axios from "axios";
 
 function BasicExample() {
-  const [inputs, setInputs] = useState({});
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    name: "",
+    surname: "",
+    age: "",
+    gender: "",
+    email: "",
+    password: "",
+    phone: "",
+    medicalaid: "",
+  });
 
   const handleChange = (event) => {
     const name = event.target.name;
@@ -15,42 +27,101 @@ function BasicExample() {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(inputs);
+
+    axios
+      .post("http://localhost:8888/api/addUser.php", inputs)
+      .then(function (response) {
+        console.log(response);
+
+        if (response.status === 200) {
+          navigate("/");
+        }
+      });
   };
   return (
     <>
       <h1>Register below and get the help you deserve</h1>
-      <Form className="reg-form" onSubmit={handleSubmit}>
+      <Form className="reg-form">
         <Form.Group className="mb-2 name-reg" controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="email" placeholder="Enter your name" />
+          <Form.Control
+            name="name"
+            type="text"
+            placeholder="please Enter your name"
+            onChange={handleChange}
+          />
         </Form.Group>
-        <Form.Group className="mb-2 surname-reg" controlId="formBasicEmail">
+        <Form.Group className="mb-2 surname-reg">
           <Form.Label>Surname</Form.Label>
-          <Form.Control type="email" placeholder="Enter your surname" />
+          <Form.Control
+            name="surname"
+            type="text"
+            placeholder="Enter your surname"
+            onChange={handleChange}
+          />
         </Form.Group>
-        <Form.Group className="mb-2 age-reg" controlId="formBasicEmail">
+        <Form.Group className="mb-2 age-reg">
           <Form.Label>Age</Form.Label>
-          <Form.Control type="email" placeholder="Enter your age" />
+          <Form.Control
+            name="age"
+            type="text"
+            placeholder="Please enter your age"
+            onChange={handleChange}
+          />
         </Form.Group>
-        <Form.Group className="mb-2 gender-reg" controlId="formBasicEmail">
+        <Form.Group className="mb-2 gender-reg">
           <Form.Label>Gender</Form.Label>
-          <Form.Control type="email" placeholder="Enter your gender" />
+          <Form.Control
+            name="gender"
+            type="text"
+            placeholder="Enter your gender"
+            onChange={handleChange}
+          />
         </Form.Group>
-        <Form.Group className="mb-2 email-reg" controlId="formBasicEmail">
+        <Form.Group className="mb-2 email-reg">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            name="email"
+            type="email"
+            placeholder="Please enter email"
+            onChange={handleChange}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3 password-reg" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
         </Form.Group>
-        <Form.Group className="mb-2 aid-reg" controlId="formBasicEmail">
+        <Form.Group className="mb-3 password-reg">
+          <Form.Label>Phone number</Form.Label>
+          <Form.Control
+            name="phone"
+            type="number"
+            placeholder="Phone number"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-2 aid-reg">
           <Form.Label>Medical Aid</Form.Label>
-          <Form.Control type="email" placeholder="Medical aid" />
+          <Form.Control
+            name="medicalaid"
+            type="email"
+            placeholder="Medical aid"
+            onChange={handleChange}
+          />
         </Form.Group>
 
-        <Button variant="primary" type="submit" id="submit-reg">
+        <Button
+          variant="primary"
+          type="submit"
+          id="submit-reg"
+          onClick={handleSubmit}
+        >
           Submit
         </Button>
       </Form>
